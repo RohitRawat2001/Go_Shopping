74520765a5dd2f75fc576a91bdf5f83fcc56be12
RohitRawat2001 <89390966+RohitRawat2001@users.noreply.github.com>
2023-08-21 22:35:28 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-const DB = "mongodb+srv://rohitrawat:rohit123@rohit.ue9uo.mongodb.net/GoShopping?retryWrites=true&w=majority"
+const DB = "mongodb+srv://passwords.ue9uo.mongodb.net/GoShopping?retryWrites=true&w=majority"
 
 mongoose.connect(DB).then(()=>console.log("data base Connected")).catch((error)=> console.log("error" + error.message))
