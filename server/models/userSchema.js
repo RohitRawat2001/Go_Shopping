e6fc09e145cd422ee141a47952d4e2e9fcb5330b
RohitRawat2001 <89390966+RohitRawat2001@users.noreply.github.com>
2023-08-21 22:34:13 +0530

    Update userSchema.js

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 // const secretKey = process.env.KEY;
 
-const secretKey = "rohitrawatakshayrawatsdgfefgeajguegvuevkueqvjrr";
+const secretKey = "";
 
 const userSchema = new mongoose.Schema({
     fname: {
